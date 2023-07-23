1cfe7bc78444ed4d48b7a6c80b35999c05485d28
majidpawar9 <majidpawar9@gmail.com>
2023-07-23 14:37:53 -0400

    added comments

--- app/src/main/java/com/majid/cambrianapp/SignInActivity.kt
+++ app/src/main/java/com/majid/cambrianapp/SignInActivity.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 import com.majid.cambrianapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    // enabled binding in a module
+    // ActivitySignInBinding creates a binding class for each XML layout file present in that module.
     private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
 
-        // This makes the views defined in activity_main.xml accessible
+        // This makes the views defined in activity_sign_in.xml accessible
         // for the UI of this activity
         setContentView(binding.root)
 
+        // This button will redirect to SignUpActivity Layout
         binding.redSignUp.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }

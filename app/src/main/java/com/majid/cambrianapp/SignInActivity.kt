25b560bdebf5a4419d096a0ad31611bd0c3fa56a
majidpawar9 <majidpawar9@gmail.com>
2023-07-23 12:34:56 -0400

    Added redirect to SignUp layout if user is not registered

--- app/src/main/java/com/majid/cambrianapp/SignInActivity.kt
+++ app/src/main/java/com/majid/cambrianapp/SignInActivity.kt
@@ -1,11 +1,22 @@
 package com.majid.cambrianapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.majid.cambrianapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+
+        // This makes the views defined in activity_main.xml accessible
+        // for the UI of this activity
+        setContentView(binding.root)
+
+        binding.redSignUp.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/majid/cambrianapp/SignUpActivity.kt
+++ app/src/main/java/com/majid/cambrianapp/SignUpActivity.kt
@@ -2,10 +2,16 @@ package com.majid.cambrianapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.majid.cambrianapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        // This makes the views defined in activity_main.xml accessible
+        // for the UI of this activity
+        setContentView(binding.root)
     }
 }
